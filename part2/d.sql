f955ee14d11721cc3b87c3c018cfbc287fa64aed
Robert <robharmony@hotmail.com>
2024-03-24 10:06:20 -0400

    sql assignment done

--- part2/d.sql
+++ part2/d.sql
@@ -1,4 +1,4 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select *
-from dogs;
+select first_name, last_name
+from users JOIN favorites ON dog_id = 2 AND user_id= users.id;
